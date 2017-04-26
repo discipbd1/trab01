d96d7d9227438e88ae8326a2b8db6a7b1ada6ceb
Yan de Paula <yanw100@gmail.com>
2017-04-26 08:19:43 -0300

    Criado arquivo com todas as tabelas.

---
+++ Padaria/tabelas/tabelas ipao.sql
@@ -0,0 +1,111 @@
+CREATE TABLE usuario(
+	cpf INT,
+	nome VARCHAR(45),
+	rg INT,
+	telefone INT,
+	email VARCHAR(45)
+);
+
+CREATE TABLE produtos_pedido(
+	id_pedido INT,
+	id_produto INT,
+	qtd INT
+);
+
+CREATE TABLE produto(
+	id_produto INT,
+	id_padaria INT,
+	nome VARCHAR(45),
+	descricao TEXT,
+	valor_unit MONEY,
+	quantidade INT
+);
+
+CREATE TABLE pedido(
+	id_pedido INT,
+	cpf INT,
+	id_padaria INT,
+	valor_total MONEY,
+	data DATE,
+	hora TIME
+);
+
+CREATE TABLE padaria_rating(
+	id_padaria INT,
+	cpf INT,
+	id_pedido INT,
+	estrelas INT,
+	recomenda INT,
+	opiniao TEXT
+);
+
+CREATE TABLE padaria(
+	id_padaria INT,
+	cpf INT,
+	nome VARCHAR(50),
+	cnpj INT,
+	email VARCHAR(50),
+	celular INT,
+	telefone INT,
+	hora_abre TIME,
+	hora_fecha TIME,
+	descricao TEXT,
+	valor_minimo MONEY,
+	foto VARCHAR
+);
+
+CREATE TABLE entrega(
+	id_entrega INT,
+	id_pedido INT,
+	hora TIME,
+	data DATE
+);
+
+CREATE TABLE endereco_usuario(
+	cpf INT,
+	estado VARCHAR(2),
+	cidade VARCHAR(30),
+	bairro VARCHAR(30),
+	cep INT,
+	rua VARCHAR(45),
+	numero INT
+);
+
+CREATE TABLE endereco_padaria(
+	id_padaria INT,
+	estado VARCHAR(2),
+	cidade VARCHAR(30),
+	bairro VARCHAR(30),
+	cep INT,
+	rua VARCHAR(45),
+	numero INT
+);
+
+CREATE TABLE endereco_dono(
+	cpf INT,
+	estado VARCHAR(2),
+	cidade VARCHAR(30),
+	bairro VARCHAR(30),
+	cep INT,
+	rua VARCHAR(45),
+	numero INT
+);
+
+CREATE TABLE dono_padaria(
+	cpf INT,
+	nome VARCHAR(45),
+	email VARCHAR(45),
+	telefone INT,
+	senha VARCHAR(45)
+);
+
+CREATE TABLE cartao(
+	cpf INT,
+	numero_cartao VARCHAR(99),
+	nome_cartao VARCHAR(99),
+	sec_cod INT,
+	primeiro_nome VARCHAR(45),
+	segundo_nome VARCHAR(60),
+	mes VARCHAR(10),
+	ano VARCHAR(10)
+);
