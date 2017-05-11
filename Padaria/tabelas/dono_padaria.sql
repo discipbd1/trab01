37ed0a5401b6177accedda39fe2017c28d93196c
Yan de Paula <yanw100@gmail.com>
2017-05-10 23:20:32 -0300

    Atualizando banco IPAO.

--- Padaria/tabelas/dono_padaria.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE dono_padaria(
-	cpf INT,
-	nome VARCHAR(45),
-	email VARCHAR(45),
-	telefone INT,
-	senha VARCHAR(45)
-)
--- Padaria/tabelas/endereco_dono.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE endereco_dono(
-	cpf INT,
-	estado VARCHAR(2),
-	cidade VARCHAR(30),
-	bairro VARCHAR(30),
-	cep INT,
-	rua VARCHAR(45),
-	numero INT
-)
