8fbf91f6ceec86e424fa1f55f352257a1e994173
Tadeujr <tadeu_junior@outlook.com>
2017-07-06 14:49:01 -0300

    sql group by

---
+++ Padaria/group_by.sql
@@ -0,0 +1,9 @@
+select nome_bairro from bairro group by nome_bairro;
+
+select primeiro_nome from cartao group by primeiro_nome;
+
+select nome from produto group by nome;
+
+select descricao from tipo_pagamento group by descricao;
+
+select nome from estado group by nome;
